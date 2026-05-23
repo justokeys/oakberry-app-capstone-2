d17bc0ed4130ce3894ac388b750fe93c580f016b
JustinK <productivekeys@gmail.com>
2026-05-22 19:23:46 -0500

    ADD PremiumCategory Enum

--- src/main/java/com/pluralsight/PremiumCategory.java
+++ src/main/java/com/pluralsight/PremiumCategory.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 public enum PremiumCategory {
+    NUT_BUTTER( Map.of(Size.SMALL, new BigDecimal("1.00"),
+            Size.MEDIUM, new BigDecimal("1.50"),
+            Size.LARGE, new BigDecimal("2.00"))),
+
+    PROTEIN(Map.of(Size.SMALL, new BigDecimal("2.00"),
+            Size.MEDIUM, new BigDecimal("3.00"),
+            Size.LARGE, new BigDecimal("4.00"))),
+
+    SUPER_FOOD( Map.of(Size.SMALL, new BigDecimal("2.50"),
+            Size.MEDIUM, new BigDecimal("3.00"),
+            Size.LARGE, new BigDecimal("4.50")));
+
+
+    private final Map<Size, BigDecimal> prices;
+
+    PremiumCategory ( Map<Size, BigDecimal> prices) {
+        this.prices = prices;
+
+    }
+
+
+
+    public BigDecimal priceFor(Size size) {
+        return prices.get(size);
+    }
 }
