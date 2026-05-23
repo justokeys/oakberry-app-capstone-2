c4ca91290ab5c0484ec5ed4efb8d98cd70b71351
JustinK <productivekeys@gmail.com>
2026-05-22 19:01:12 -0500

    ADD SideType enum

--- src/main/java/com/pluralsight/SideType.java
+++ src/main/java/com/pluralsight/SideType.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
+import java.math.BigDecimal;
+
 public enum SideType {
+    OAK_BAR("OakBar", new BigDecimal("3.50")),
+    GRANOLA("Organic Granola",new BigDecimal("3.00")),
+    PEANUT_BUTTER_BITES("Organinc Peanut Bites",new BigDecimal("8.95"));
+
+    private final BigDecimal price;
+    private final String displayName;
+
+
+    SideType(String displayName,BigDecimal price) {
+        this.price = price;
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
 }
