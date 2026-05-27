87212634f047525a3bc00acde1154b3382de4b9a
JustinK <productivekeys@gmail.com>
2026-05-27 18:00:22 -0500

    add ui menu

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -1,49 +1,53 @@
 package ui;
 
+import com.pluralsight.Order;
+
 import java.util.Scanner;
 
 public class UI {
     private final Scanner thescanner = new Scanner(System.in);
-    boolean appRunning = true;
-    // Create the active cart for this session
-    while(appRunning){
-        System.out.println(
-                """ 
-                == Welcome to Oakberry ==
-                1- Add a Bowl
-                2- Add a Drink
-                3- View Current Order
-                4- Checkout
-                5- Exit
-                Please choose an option: \s""" );
+    private final Order currentOrder = new Order();
+    private boolean AppRunning;
 
-        int menuChoice = thescanner.nextInt();
-        thescanner.nextLine();
-        switch (menuChoice) {
-            case 1:
-                acaiMenu();
-                break;
-            case 2:
-                drinkMenu();
-                break;
-            case 3:
-                displayCart();
-                break;
-            case 4:
-                checkOut();
-                break;
-            case 5:
-                System.out.println("Thank you !");
-                appRunning = false;
-                break;
-            default:
-                System.out.println("Invalid selection. Please try again.");
+    public void start() {
+        boolean appRunning = true;
+        // Create the active cart for this session
+        while (appRunning) {
+            System.out.println(
+                    """ 
+                            == Welcome to Oakberry ==
+                            1- Add a Bowl
+                            2- Add a Drink
+                            3- View Current Order
+                            4- Checkout
+                            5- Exit
+                            Please choose an option: \s""");
+
+            int menuChoice = thescanner.nextInt();
+            thescanner.nextLine();
+            switch (menuChoice) {
+                case 1:
+                    acaiMenu(currentOrder);
+                    break;
+                case 2:
+                    drinkMenu(currentOrder);
+                    break;
+                case 3:
+                    displayCart(currentOrder);
+                    break;
+                case 4:
+                    checkOut(currentOrder);
+                    break;
+                case 5:
+                    System.out.println("Thank you !");
+                    appRunning = false;
+                    break;
+                default:
+                    System.out.println("Invalid selection. Please try again.");
+            }
+
         }
-
     }
-
-
-
 
 
 }
