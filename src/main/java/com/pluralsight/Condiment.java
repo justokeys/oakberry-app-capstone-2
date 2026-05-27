6286865d70d3dea4de8c23e9662979e7fa629d26
JustinK <productivekeys@gmail.com>
2026-05-27 11:24:53 -0500

    add condiment and toppings to topping methods

--- src/main/java/com/pluralsight/Condiment.java
+++ src/main/java/com/pluralsight/Condiment.java
@@ -1,12 +1,17 @@
 package com.pluralsight;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class Condiment extends Topping{
     public Condiment( String name) {
-        super(name);
+        super("Drizzle");
 
     }
+    private static final List<Condiment> MENU = List.of(
+            new Condiment("Honey"),
+            new Condiment("Peanut butter")
+    );
 
     @Override
     BigDecimal getPriceForSize(Size size) {
--- src/main/java/com/pluralsight/RegularTopping.java
+++ src/main/java/com/pluralsight/RegularTopping.java
@@ -1,11 +1,32 @@
 package com.pluralsight;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class RegularTopping extends Topping {
     public RegularTopping( String name) {
         super(name);
     }
+    private static final List<RegularTopping> MENU = List.of(
+            new RegularTopping("Strawberry"),
+            new RegularTopping("Banana"),
+            new RegularTopping("Blueberry"),
+            new RegularTopping("Passion Fruit"),
+            new RegularTopping("Cocao Nibbs"),
+            new RegularTopping("Granola"),
+            new RegularTopping("Chia Pudding"),
+            new RegularTopping("Chia Seeds"),
+            new RegularTopping("Coconut Flakes"),
+            new RegularTopping("Peanut Butter"),
+            new RegularTopping("Almond Flakes"),
+            new RegularTopping("Honey"),
+            new RegularTopping("Goji Berries"),
+            new RegularTopping("Vegan Granola"),
+            new RegularTopping("Milk Powder")
+
+    );
 
 
     @Override
